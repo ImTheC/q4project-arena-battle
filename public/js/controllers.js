65997d9b2ce698e74f46858a6519730bf190edad
Christopher Anderson <Christopher@ImTheC.com>
2016-10-09 17:41:20 -0500

    Initial Angular setup

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,17 +1,8 @@
 app.controller('MainController', function($scope) {
   $scope.view = {};
 
-	$scope.view.result = "";
-
 	$scope.view.formData = {};
 	$scope.view.boom = "boom"
-	$scope.view.sortBy = "-votes";
-	$scope.view.sortTitle = " Votes - High to Low";
-	$scope.view.searchBy = "$";
-	$scope.view.searchTitle = " All";
 
-	$scope.view.newPost = false;
-	$scope.view.newComment = {};
-	$scope.view.postId = 4;
 
 });
